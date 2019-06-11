32ca81c0ceab4099f246c59be0d0df0a5c7f433f
Thomas Reisenegger <treisenegger@github.com>
2019-06-11 19:34:36 -0400

    Add initial style

---
+++ src/app/word/word.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-word',
+  templateUrl: './word.component.html',
+  styleUrls: ['./word.component.scss']
+})
+export class WordComponent implements OnInit {
+  @Input() word: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
