b78e73900dc1ae9a7932be4fe52f7656c4b61bae
Thomas Reisenegger <treisenegger@github.com>
2019-06-11 20:58:16 -0400

    Detect whether guess is correct

--- src/app/word/word.component.ts
+++ src/app/word/word.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {Store, Select} from '@ngxs/store';
+import {Observable} from 'rxjs';
+import {TakeGuess} from '../../store/words/words.actions';
+import {WordsState} from '../../store/words/words.state';
 
 @Component({
   selector: 'app-word',
@@ -6,11 +10,29 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./word.component.scss']
 })
 export class WordComponent implements OnInit {
+  guess: string;
+  correct: boolean;
   @Input() word: string;
+  @Select(WordsState.getGuess) guessO: Observable<string>;
+  @Select(WordsState.getCorrect) correctO: Observable<boolean>;
 
-  constructor() { }
+  constructor(private store: Store) { }
 
   ngOnInit() {
+    this.guessO.subscribe(newGuess => this.guess = newGuess);
+    this.correctO.subscribe(newCorrect => this.correct = newCorrect);
+  }
+
+  takeGuess() {
+    this.store.dispatch(new TakeGuess({ guess: this.word }));
+  }
+
+  setClasses() {
+    const classes = {
+      'is-correct': this.word === this.guess && this.correct,
+      'is-incorrect': this.word === this.guess && !this.correct
+    }
+    return classes;
   }
 
 }
